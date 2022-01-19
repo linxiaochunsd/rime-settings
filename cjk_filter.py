fa74288894d39d1c5852931f907509602685360e
linxiaochun <linxiaochun@baidu.com>
2022-01-19 17:17:43 +0800

    fix

--- cjk_filter.py
+++ cjk_filter.py
@@ -5,13 +5,11 @@ font = TTFont(file='/System/Library/Fonts/STHeiti Light.ttc',fontNumber=1)
 unicode_map = font['cmap'].tables[0].ttFont.getBestCmap()
 glyf_map = font['glyf']
 dic_list = [
-    # "easy_en.dict.yaml",
-    # "essay.dict.yaml",
+    "essay.txt",
     # "luna_pinyin.chat.dict.yaml",
     # "luna_pinyin.chengyusuyu.dict.yaml",
     # "luna_pinyin.computer.dict.yaml",
-    "luna_pinyin.dict.yaml",
-    # "luna_pinyin_simp.dict.yaml",
+    # "luna_pinyin.dict.yaml",
     # "luna_pinyin.extended.dict.yaml",
     # "luna_pinyin.kaifa.dict.yaml",
     # "luna_pinyin.mingxing.dict.yaml",
@@ -25,13 +23,14 @@ dic_list = [
 ok = 0
 bad = 0
 import opencc
-converter = opencc.OpenCC('t2s.json')
+t2s = opencc.OpenCC('t2s.json')
+s2t = opencc.OpenCC('s2t.json')
 for dic in dic_list:
     o = open("output/"+dic,"a")
     comment_end=False
     with open(dic,"r") as f:
         for line in f.readlines():
-            print("ok",ok,"bad",bad,"line:",line)
+    #        print("ok",ok,"bad",bad,"line:",line)
             splits = line.split()
             if len(splits) == 0:
                 o.writelines(line)
@@ -40,21 +39,27 @@ for dic in dic_list:
             isbad = False
             for word in words:
                 if not (ord(word) in unicode_map and len(glyf_map[unicode_map[ord(word)]].getCoordinates(0)[0]) > 0):
-                    # print(f'字体库中有：【{word}】这个汉字')
+                    print("ok",ok,"bad",bad,"line:",line, words)
                     bad += 1
                     isbad = True
                     break
-                # print(f'字体库没有：【{word}】这个汉字')
 
             if not isbad:
-                words = converter.convert(words)
+                words = t2s.convert(words)
                 for word in words:
                     if not (ord(word) in unicode_map and len(glyf_map[unicode_map[ord(word)]].getCoordinates(0)[0]) > 0):
-                        # print(f'字体库中有：【{word}】这个汉字')
+                        print("ok",ok,"bad",bad,"line:",line, words)
                         bad += 1
                         isbad = True
                         break
-
+            if not isbad:
+                words = s2t.convert(words)
+                for word in words:
+                    if not (ord(word) in unicode_map and                                len(glyf_map[unicode_map[ord(word)]].getCoordinates(0)[0]) > 0):
+                        print("ok",ok,"bad",bad,"line:",line, words)
+                        bad += 1
+                        isbad = True
+                        break
             if not isbad:
                 o.writelines(line)
                 ok += 1
